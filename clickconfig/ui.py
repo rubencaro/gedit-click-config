3a6253f7a9c62b81c7a8f69ce154f56b726920d5
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-10-31 10:31:31 +0100

    Make it load

--- clickconfig/ui.py
+++ clickconfig/ui.py
@@ -46,22 +46,22 @@ from gi.repository import Gtk
 
 from .logger import Logger
 LOGGER = Logger(level=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')[2])
-import dialogs
+import clickconfig.dialogs
 
 class ConfigUI(object):
-    
+
     """
     The configuration window for Click_Config.
-    
+
     Usage:
     config_ui = ConfigUI()
     config_ui.window.show()
-    
+
     See:
     click_config.py ClickConfigPlugin.create_configure_dialog()
-    
+
     """
-    
+
     def __init__(self, plugin):
         """
         1. Create the window.
@@ -71,7 +71,7 @@ class ConfigUI(object):
         """
         self._plugin = plugin
         LOGGER.log()
-        
+
         # 1. Create the window.
         glade_file = os.path.join(self._plugin.plugin_path, 'Click_Config.ui')
         self.builder = Gtk.Builder()
@@ -82,14 +82,14 @@ class ConfigUI(object):
         self.window.set_destroy_with_parent(True)
         self.window.set_position(Gtk.WindowPosition.CENTER_ON_PARENT)
         self.language_dialog = None
-        
+
         # 2. Make a temporary copy of the configuration.
         self._mod_conf = self._plugin.conf.copy()
         self.preserved_sets = [item.name for item in
             self._mod_conf.configsets if item.preserved]
         self.preserved_ops = [item.name for item in
             self._mod_conf.ops if item.preserved]
-        
+
         # 3. Update the window's widgets to reflect the configuration.
         width = self._mod_conf.window_width
         if width:
@@ -99,7 +99,7 @@ class ConfigUI(object):
         self._update_define_combobox()
         self._update_define_display()
         self._update_apply_button()
-    
+
         # 4. Connect the event handlers.
         signals_to_actions = {
             'on_config_window_destroy':
@@ -152,21 +152,21 @@ class ConfigUI(object):
                 self.on_Import_button_clicked,
             }
         self.builder.connect_signals(signals_to_actions)
-        
+
         LOGGER.log('Configuration window opened.')
-        
-        
+
+
     ### 1 - General configure window
-    
+
     # 1.1 - Event handlers
-    
+
     def on_config_window_destroy(self, event):
         """Let the ClickConfigPlugin know that the ConfigUI is gone."""
         LOGGER.log()
         LOGGER.log('Configuration window closed.')
         self._plugin.config_ui = None
         return False
-    
+
     def on_OK_button_clicked(self, button):
         """
         Give the ClickConfigPlugin the modified configuration, then close.
@@ -177,23 +177,23 @@ class ConfigUI(object):
         self._plugin.update_configuration(self._mod_conf.copy())
         self.window.destroy()
         self._plugin.config_ui = None
-    
+
     def on_Apply_button_clicked(self, button):
         """Give the ClickConfigPlugin the modified configuration."""
         LOGGER.log()
         self._plugin.update_configuration(self._mod_conf.copy())
         self._update_apply_button()
-    
+
     def on_Cancel_button_clicked(self, button):
         """Close without giving ClickConfigPlugin the modified configuration."""
         LOGGER.log()
         self.window.destroy()
-    
+
     def on_Browse_button_clicked(self, button):
         """Browse to the configuration file."""
         LOGGER.log()
         self._plugin.open_config_dir()
-    
+
     def on_Import_button_clicked(self, button):
         """Import ConfigSets and SelectionOps from another configuration file."""
         LOGGER.log()
@@ -206,9 +206,9 @@ class ConfigUI(object):
         self._update_define_combobox()
         self._update_define_display()
         self._update_apply_button()
-    
+
     # 1.2 - Support functions
-    
+
     def _update_apply_button(self):
         """Correct the Apply button's sensitivity."""
         LOGGER.log()
@@ -216,7 +216,7 @@ class ConfigUI(object):
         has_changes = self._mod_conf != self._plugin.conf
         LOGGER.log(var='has_changes')
         apply_button.set_sensitive(has_changes)
-    
+
     def _filechooser_dialog(self, title='Open'):
         """
         Provide file selection dialog to user and return the selected filename.
@@ -246,11 +246,11 @@ class ConfigUI(object):
             filename = ''
         dialog.destroy()
         return filename
-    
+
     ### 2 - ConfigSet name section
-    
+
     # 2.1 - Event handlers
-    
+
     def on_config_combobox_changed(self, combobox):
         """Update the configuration and interface based on the selection."""
         LOGGER.log()
@@ -265,7 +265,7 @@ class ConfigUI(object):
         # Update interface
         self._update_config_display()
         config_remove_button.set_sensitive(is_removable)
-    
+
     def on_comboboxentryentry1_key_press_event(self, widget, event):
         """React to the Enter key here the same as for the Add button."""
         LOGGER.log()
@@ -273,7 +273,7 @@ class ConfigUI(object):
         keyval_name = Gdk.keyval_name(keyval)
         if keyval_name in ('Return', 'KP_Enter'):
             self._add_config()
-    
+
     def on_config_add_button_clicked(self, button):
         """Call function to add the configuration."""
         LOGGER.log()
@@ -283,13 +283,13 @@ class ConfigUI(object):
         """Call function to remove the configuration."""
         LOGGER.log()
         self._remove_config()
-    
+
     def on_languages_button_clicked(self, button):
         """Call function for the languages configuration dialog."""
         LOGGER.log()
         language_window = self._create_languages_dialog()
         language_window.show()
-    
+
     def _create_languages_dialog(self):
         """Display the languages configuration dialog."""
         LOGGER.log()
@@ -298,9 +298,9 @@ class ConfigUI(object):
         else:
             self.language_dialog = dialogs.LanguageDialog(self._plugin)
         return self.language_dialog.window
-    
+
     # 2.2 - Support functions
-    
+
     def _update_config_combobox(self):
         """Reflect the ConfigSets and current ConfigSet in the interface."""
         LOGGER.log()
@@ -312,7 +312,7 @@ class ConfigUI(object):
         self._fill_combobox(config_combobox, combobox_list)
         config_combobox.set_id_column(0)
         config_combobox.set_active_id(self._mod_conf.current_configset_name)
-    
+
     def _row_separator_func(self, model, iter_, data=None):
         """Identify what item represents a separator."""
         LOGGER.log()
@@ -325,7 +325,7 @@ class ConfigUI(object):
         configset_names = [item.name for item in self._mod_conf.configsets]
         configset_names = configset_names[0:2] + sorted(configset_names[2:])
         return configset_names
-    
+
     def _add_config(self):
         """Add the configuration."""
         LOGGER.log()
@@ -348,7 +348,7 @@ class ConfigUI(object):
         self._update_config_combobox()
         self._update_config_display()
         LOGGER.log('ConfigSet added: %s.' % configset_name)
-    
+
     def _get_new_name(self, name='', existing_names=(), desc=''):
         """Ask the user for a new name, and ensure it is new."""
         while 1:
@@ -365,7 +365,7 @@ class ConfigUI(object):
                                  type_=Gtk.MessageType.ERROR,
                                  parent=self.window)
         return name
-    
+
     def _remove_config(self):
         """Remove the configuration."""
         LOGGER.log()
@@ -389,17 +389,17 @@ class ConfigUI(object):
             self._update_config_combobox()
             self._update_config_display()
             LOGGER.log('ConfigSet removed: %s.' % config_name)
-    
+
     def _is_config_name_removable(self, config_name):
         """Check if ConfigSet of this name can be removed."""
         LOGGER.log()
         return (config_name in self._mod_conf.get_configset_names() and
                      config_name not in self.preserved_sets)
-    
+
     ### 3 - ConfigSet settings section
-    
+
     # 3.1 - Event handlers
-    
+
     def on_combobox_changed(self, combobox):
         """
         Update the configuration and interface to reflect the SelectionOp name.
@@ -421,7 +421,7 @@ class ConfigUI(object):
         # Make sure a typed-but-not-added config name isn't showing
         if entry_config_name != self._mod_conf.current_configset_name:
             self._update_config_combobox()
-    
+
     def on_edit_clicked(self, button):
         """Set the selection for this n-click as the current selection."""
         LOGGER.log()
@@ -435,9 +435,9 @@ class ConfigUI(object):
         self._update_apply_button()
         self._update_define_combobox()
         self._update_define_display()
-    
+
     # 3.2 - Support functions
-    
+
     def _fill_combobox(self, combobox, items):
         """Put a list of the SelectionOp names in the combobox."""
         LOGGER.log()
@@ -445,7 +445,7 @@ class ConfigUI(object):
         for item in items:
             combobox_liststore.append([item])
         combobox.set_model(combobox_liststore)
-    
+
     def _update_config_display(self):
         """
         Reflect the five SelectionOps of the current ConfigSet in the widgets.
@@ -497,11 +497,11 @@ class ConfigUI(object):
         objects['m'].set_sensitive(False)
         objects['s'].set_sensitive(False)
         objects['x'].set_sensitive(False)
-        
+
     ### 4 - Define section
-    
+
     # 4.1 - Event handlers
-    
+
     def on_define_combobox_changed(self, combobox):
         """Update the configuration and interface for the SelectionOp name."""
         LOGGER.log()
@@ -511,12 +511,12 @@ class ConfigUI(object):
             self._mod_conf.current_op_name = op_name
             self._update_apply_button()
         self._update_define_display()
-    
+
     def on_define_changed(self, editable):
         """Call function to update the Add button."""
         LOGGER.log()
         self._update_define_save_button()
-    
+
     def on_define_entry_key_press_event(self, widget, event):
         """React to the Enter key here the same as for the Add button."""
         LOGGER.log()
@@ -524,24 +524,24 @@ class ConfigUI(object):
         keyval_name = Gdk.keyval_name(keyval)
         if keyval_name in ('Return', 'KP_Enter'):
             self._add_define()
-    
+
     def on_define_add_button_clicked(self, button):
         """Call function to add a new SelectionOp with the current pattern."""
         LOGGER.log()
         self._add_define()
-    
+
     def on_define_remove_button_clicked(self, button):
         """Call function to remove the current SelectionOp."""
         LOGGER.log()
         self._remove_define()
-    
+
     def on_define_save_button_clicked(self, button):
         """Call function to update the SelectionOp for the changed pattern."""
         LOGGER.log()
         self._save_define()
-    
+
     # 4.2 - Support functions
-    
+
     def _update_define_combobox(self):
         """Reflect the SelectionOps and current SelectionOp in the combobox."""
         LOGGER.log()
@@ -551,7 +551,7 @@ class ConfigUI(object):
         combobox.set_id_column(0)
         op_name = self._mod_conf.current_op_name
         combobox.set_active_id(op_name)
-    
+
     def _update_define_display(self):
         """Reflect the current SelectionOp in the interface."""
         LOGGER.log()
@@ -567,7 +567,7 @@ class ConfigUI(object):
         self._set_define_in_ui(op.pattern, op.flags, is_editable)
         save_button.set_sensitive(False)
         remove_button.set_sensitive(is_editable)
-    
+
     def _get_definition_objects(self):
         """Get a dictionary of the widgets that describe the selection."""
         LOGGER.log()
@@ -581,7 +581,7 @@ class ConfigUI(object):
         ]
         objects = dict((k, self.builder.get_object(n)) for k, n in names)
         return objects
-    
+
     def _get_define_in_ui(self):
         """Get the pattern, and flags from the define objects."""
         LOGGER.log()
@@ -594,7 +594,7 @@ class ConfigUI(object):
                  objects['s'].get_active() * re.S +
                  objects['x'].get_active() * re.X)
         return pattern, flags
-    
+
     def _set_define_in_ui(self, pattern, flags, is_editable=True):
         """Set the pattern, and flags of the define objects."""
         LOGGER.log()
@@ -611,7 +611,7 @@ class ConfigUI(object):
         objects['m'].set_sensitive(is_editable)
         objects['s'].set_sensitive(is_editable)
         objects['x'].set_sensitive(is_editable)
-    
+
     def _update_define_save_button(self):
         """Correct the Save button's sensitivity for the pattern and flags."""
         LOGGER.log()
@@ -628,7 +628,7 @@ class ConfigUI(object):
         is_preserved_op = op_name in self.preserved_ops
         # Update interface
         save_button.set_sensitive(has_changes and not is_preserved_op)
-    
+
     def _add_define(self):
         """Add a new SelectionOp."""
         LOGGER.log()
@@ -653,7 +653,7 @@ class ConfigUI(object):
         self._update_config_display()
         self._update_define_combobox()
         LOGGER.log('SelectionOp added: %s.' % op_name)
-    
+
     def _save_define(self):
         """Update the current SelectionOp."""
         LOGGER.log()
@@ -670,7 +670,7 @@ class ConfigUI(object):
         self._update_config_display()
         self._update_define_save_button()
         LOGGER.log('SelectionOp saved: %s.' % op_name)
-    
+
     def _save_op(self, op_name, pattern, flags):
         """Add or update a SelectionOp."""
         LOGGER.log()
@@ -683,7 +683,7 @@ class ConfigUI(object):
         new_op.flags = flags
         self._mod_conf.add_op(new_op)
         return True
-    
+
     def _is_valid_re(self, pattern, flags):
         """
         Check the validity of the regular expression and
@@ -692,7 +692,7 @@ class ConfigUI(object):
         LOGGER.log()
         try:
             is_valid = bool(re.compile(pattern, flags))
-        except re.error, re_error:
+        except (re.error, re_error):
             is_valid = False
             title = 'Click Config: error in input'
             flag_text =  '\n    I (IGNORECASE)' * bool(flags & re.I)
@@ -710,7 +710,7 @@ class ConfigUI(object):
                                  Gtk.MessageType.ERROR,
                                  self.window)
         return is_valid
-    
+
     def _remove_define(self):
         """Select the preceding SelectionOp and remove the current one."""
         LOGGER.log()
@@ -738,5 +738,3 @@ class ConfigUI(object):
         self._update_config_display()
         self._update_define_combobox()
         LOGGER.log('SelectionOp removed: %s.' % op_name)
-    
-
